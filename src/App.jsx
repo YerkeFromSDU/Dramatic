72f8fedbf767d64a603cde19b874c0eb07527904
YerkeFromSDU <190103406@stu.sdu.edu.kz>
2023-05-17 13:17:32 +0600

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App(){
     <BrowserRouter>
       <Routes>
         <Route path="/trailer" element={<Trailer />}/>
-        <Route path="/home" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
       </Routes>
     </BrowserRouter>
